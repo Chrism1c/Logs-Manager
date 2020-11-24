04c56cd90be94201b300be2594c68090dd80af3b
chrism1c <christian-miccolis@libero.it>
2020-11-24 14:15:46 +0100

    Aggiunte le Console label

--- src/gui_interface.py
+++ src/gui_interface.py
@@ -39,79 +39,118 @@ class Ui(QtWidgets.QDialog):
         self.show()
 
     def fileDialog(self, value):
-        filenames, x = QFileDialog.getOpenFileNames(self, "QFileDialog.getOpenFileNames()", "",
-                                                    "Cartella di lavoro Excel (*.xlsx);;Cartella di lavoro Excel "
-                                                    "97-2003 (*.xls);;CSV (Delimitato dal separatore di elenco) ("
-                                                    "*.csv)")
-        if value:
-            self.concat_input_text.setPlainText("\n".join(filenames))
-            self.listof_concatFiles = filenames
-        else:
-            self.merge_input_text.setPlainText("\n".join(filenames))
-            self.listof_mergeFiles = filenames
 
-            ext_in = findExtension(x)
-            frames = loadLogs(filenames, ext_in)
+        self.Console_merge.setText("")
+        self.Console_merge.setStyleSheet("background-color: none;")
+        self.Console_concat.setText("")
+        self.Console_concat.setStyleSheet("background-color: none;")
 
-            leftColumns, rightColumns = getHeaders(frames)
-            for elem in leftColumns:
-                self.comboBox_leftKey.addItem(elem)
+        try:
+            filenames, x = QFileDialog.getOpenFileNames(self, "Select input files (2+ Concat | Only 2 Merge)", "",
+                                                        "Cartella di lavoro Excel (*.xlsx);;Cartella di lavoro Excel "
+                                                        "97-2003 (*.xls);;CSV (Delimitato dal separatore di elenco) ("
+                                                        "*.csv)")
+            if value:
+                self.concat_input_text.setPlainText("\n".join(filenames))
+                self.listof_concatFiles = filenames
+            else:
+                self.merge_input_text.setPlainText("\n".join(filenames))
+                self.listof_mergeFiles = filenames
 
-            for elem in rightColumns:
-                self.comboBox_rightKey.addItem(elem)
+                ext_in = findExtension(x)
+                frames = loadLogs(filenames, ext_in)
 
-        print(filenames)
+                leftColumns, rightColumns = getHeaders(frames)
+                for elem in leftColumns:
+                    self.comboBox_leftKey.addItem(elem)
+
+                for elem in rightColumns:
+                    self.comboBox_rightKey.addItem(elem)
+            print(filenames)
+        except:
+            print("***** Errore in fileDialog")
+            self.merge_input_text.setPlainText("")
+            self.concat_input_text.setPlainText("")
+            self.listof_concatFiles.clear
+            self.listof_mergeFiles.clear
 
     def fileDialogSave(self, value):
-        filename, x = QFileDialog.getSaveFileName(self, "QFileDialog.getOpenFileNames()", "",
-                                                  "Cartella di lavoro Excel (*.xlsx);;Cartella di lavoro Excel "
-                                                  "97-2003 (*.xls);;CSV (Delimitato dal separatore di elenco) ("
-                                                  "*.csv)")
-        if value:
-            self.concat_output_text.setPlainText(filename)
-        else:
-            self.merge_output_text.setPlainText(filename)
 
-        self.output_path = filename
-        print(filename)
+        self.Console_merge.setText("")
+        self.Console_merge.setStyleSheet("background-color: none;")
+        self.Console_concat.setText("")
+        self.Console_concat.setStyleSheet("background-color: none;")
+
+        try:
+            filename, x = QFileDialog.getSaveFileName(self, "Save file as:", "",
+                                                      "Cartella di lavoro Excel (*.xlsx);;Cartella di lavoro Excel "
+                                                      "97-2003 (*.xls);;CSV (Delimitato dal separatore di elenco) ("
+                                                      "*.csv)")
+            if value:
+                self.concat_output_text.setPlainText(filename)
+            else:
+                self.merge_output_text.setPlainText(filename)
+
+            self.output_path = filename
+            print(filename)
+        except:
+            print("***** Errore in fileDialogSave")
+            self.concat_output_text.setPlainText("")
+            self.output_path = ""
 
     def concatNow(self):
-        if len(self.listof_concatFiles) > 1 and len(self.output_path) > 1:
+        try:
+            if len(self.listof_concatFiles) > 1 and len(self.output_path) > 1:
 
-            ext_in = findExtension(self.listof_concatFiles[0])
-            ext_out = findExtension(self.output_path)
+                ext_in = findExtension(self.listof_concatFiles[0])
+                ext_out = findExtension(self.output_path)
 
-            concateneteLogs(self.listof_concatFiles, self.output_path, ext_in, ext_out)
+                concateneteLogs(self.listof_concatFiles, self.output_path, ext_in, ext_out)
 
-            print('Concat succesful !')
-            self.listof_concatFiles.clear()
-            self.output_path = ""
-            self.concat_input_text.setPlainText("")
-            self.concat_output_text.setPlainText("")
-        else:
-            print('Concat failed !')
+                print('Concat succesful !')
+                self.Console_concat.setText("Concat succesful !")
+                self.Console_concat.setStyleSheet("background-color: lightgreen;")
+            else:
+                print('Concat failed !\n NB:\n- Select two ore more input files [OPEN] \n- Define one output file ['
+                      'SAVE]')
+                self.Console_concat.setText("Concat failed !")
+                self.Console_concat.setStyleSheet("background-color: red;")
+        except:
+            print("***** Errore in concatNow")
+
+        self.listof_concatFiles.clear()
+        self.output_path = ""
+        self.concat_input_text.setPlainText("")
+        self.concat_output_text.setPlainText("")
 
     def mergeNow(self):
+        try:
+            # Controllo chiavi
+            lkey = self.comboBox_leftKey.currentText()
+            rkey = self.comboBox_rightKey.currentText()
+            print("Chiave left corrente = ", self.comboBox_leftKey.currentText())
+            print("Chiave right corrente = ", self.comboBox_rightKey.currentText())
 
-        # Controllo chiavi
-        lkey = self.comboBox_leftKey.currentText()
-        rkey = self.comboBox_rightKey.currentText()
-        print("Chiave left corrente = ", self.comboBox_leftKey.currentText())
-        print("Chiave right corrente = ", self.comboBox_rightKey.currentText())
+            if len(self.listof_mergeFiles) == 2 and len(self.output_path) > 0:
 
-        if len(self.listof_mergeFiles) == 2 and len(self.output_path) > 0:
+                ext_in = findExtension(self.listof_mergeFiles[0])
+                ext_out = findExtension(self.output_path)
 
-            ext_in = findExtension(self.listof_mergeFiles[0])
-            ext_out = findExtension(self.output_path)
+                mergeLogs(lkey, rkey, self.listof_mergeFiles, self.output_path, ext_in, ext_out)
 
-            mergeLogs(lkey, rkey, self.listof_mergeFiles, self.output_path, ext_in, ext_out)
+                print('Merge succesful !')
+                self.Console_merge.setText("Merge succesful !")
+                self.Console_merge.setStyleSheet("background-color: lightgreen;")
+            else:
+                print('Merge failed !\n NB:\n- Select only two input files [OPEN] \n- Define one output file [SAVE]')
+                self.Console_merge.setText("Merge failed !")
+                self.Console_merge.setStyleSheet("background-color: red;")
+        except:
+            print("***** Errore in mergeNow")
 
-            print('Merge succesful !')
-            self.listof_mergeFiles.clear()
-            self.output_path = ""
-            self.merge_input_text.setPlainText("")
-            self.merge_output_text.setPlainText("")
-            self.comboBox_leftKey.clear()
-            self.comboBox_rightKey.clear()
-        else:
-            print('Merge failed !')
+        self.listof_mergeFiles.clear()
+        self.output_path = ""
+        self.merge_input_text.setPlainText("")
+        self.merge_output_text.setPlainText("")
+        self.comboBox_leftKey.clear()
+        self.comboBox_rightKey.clear()
