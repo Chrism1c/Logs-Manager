59825c582199a380f61bd83d50f8cecc641e6b1f
chrism1c <christian-miccolis@libero.it>
2021-01-24 18:10:51 +0100

    fix README

--- 12_InstallLibraries.cmd
+++
@@ -1,9 +0,0 @@
-:: mkdir dependencies
-:: pip download -r requirements.txt -d "./dependencies"
-:: tar cvfz dependencies.tar.gz dependencies
-
-::tar zxvf dependencies.tar.gz
-@ECHO OFF
-cd src\req\dependencies
-pip install --no-index --find-links=wheels/ -r requirements.txt
-EXIT /B
